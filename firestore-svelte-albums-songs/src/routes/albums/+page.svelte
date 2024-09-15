91e688c514d53778c0c19d38801a4018b40e88f6
long.phan <dragons3232@gmail.com>
2024-09-15 18:12:23 +0700

    Collection store

--- firestore-svelte-albums-songs/src/routes/albums/+page.svelte
+++ firestore-svelte-albums-songs/src/routes/albums/+page.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-	import { Collection } from 'sveltefire';
+	import { getFirebaseContext, collectionStore } from 'sveltefire';
+	const { auth, firestore, storage, rtdb, analytics } = getFirebaseContext();
+
+	const data = collectionStore(firestore, 'albums');
 </script>
 
-<Collection ref={'albums'} let:data let:count>
-	<p>Found {count} albums</p>
-
-	{#each data as album}
-		<p>{album.name}</p>
-		<p>{album.desc}</p>
-		<p>{album.date}</p>
-		<p>{new Date(album.date.seconds * 1000)}</p>
-	{/each}
-
-	<p slot="loading">Loading...</p>
-</Collection>
+{#each $data as album}
+	<p>{album.name}</p>
+	<p>{album.desc}</p>
+	<p>{album.date}</p>
+	<p>{new Date(album.date.seconds * 1000)}</p>
+{/each}
