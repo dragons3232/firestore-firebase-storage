af35fc5d69cbd38a532ea6a6e456f19801285a27
long.phan <dragons3232@gmail.com>
2024-09-15 17:58:32 +0700

    Collection store

---
+++ firestore-svelte-albums-songs/src/routes/albums/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Collection } from 'sveltefire';
+</script>
+
+<Collection ref={'albums'} let:data let:count>
+	<p>Found {count} albums</p>
+
+	{#each data as album}
+		<p>{album.name}</p>
+		<p>{album.desc}</p>
+		<p>{album.date}</p>
+		<p>{new Date(album.date.seconds * 1000)}</p>
+	{/each}
+
+	<p slot="loading">Loading...</p>
+</Collection>
