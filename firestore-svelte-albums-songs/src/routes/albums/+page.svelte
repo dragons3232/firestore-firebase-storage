57d228661072236d30f99b5e7c4a16e166c8e5ff
long.phan <dragons3232@gmail.com>
2024-09-15 18:41:44 +0700

    Add document

--- firestore-svelte-albums-songs/src/routes/albums/+page.svelte
+++ firestore-svelte-albums-songs/src/routes/albums/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+	import { addDoc, collection } from 'firebase/firestore';
 	import { getFirebaseContext, collectionStore } from 'sveltefire';
 	const { auth, firestore, storage, rtdb, analytics } = getFirebaseContext();
 
 	const data = collectionStore(firestore, 'albums');
+	const albumCollection = collection(firestore, 'albums');
+
+	const addAlbum = async () => {
+		await addDoc(albumCollection, {
+			name: 'Album' + new Date().getTime(),
+			desc: 'Album Description',
+			date: new Date()
+		});
+	};
 </script>
 
 {#each $data as album}
@@ -11,3 +21,5 @@
 	<p>{album.date}</p>
 	<p>{new Date(album.date.seconds * 1000)}</p>
 {/each}
+
+<button on:click={addAlbum}>Add Album</button>
